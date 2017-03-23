ba46484f0f0d1c823554a28c30012b67967e3dd5
Richard Sudario <rsberrocal@gmail.com>
2017-03-23 20:37:39 +0100

    Node done!

--- 4-Comarques/src/Comarques/Node.java
+++ 4-Comarques/src/Comarques/Node.java
@@ -11,18 +11,33 @@ package Comarques;
  */
 public class Node {
 
-    String comarques;
-    int poblacio;
-    double superficie;
-    double densitat;
+    private String comarques;
+    private int poblacio;
+    private double superficie;
+    private double densitat;
     Node next;
 
     Node(String comarques, int poblacio, double superficie, double densitat, Node next) {
-        this.comarques=comarques;
-        this.poblacio=poblacio;
-        this.superficie=superficie;
-        this.densitat=densitat;
-        this.next=next;               
+        this.comarques = comarques;
+        this.poblacio = poblacio;
+        this.superficie = superficie;
+        this.densitat = densitat;
+        this.next = next;
     }
 
+    public String getComarques() {
+        return comarques;
+    }
+
+    public int getPoblacio() {
+        return poblacio;
+    }
+
+    public double getSuperficie() {
+        return superficie;
+    }
+
+    public double getDensitat() {
+        return densitat;
+    }
 }
