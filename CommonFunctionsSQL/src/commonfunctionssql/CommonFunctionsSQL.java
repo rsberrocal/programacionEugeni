ea09605055e63e6a7a484f43d2e8147c8b35e612
Richard Sudario <rsberrocal@gmail.com>
2017-04-30 12:00:07 +0200

    Añadidos funciones para MySQL

--- CommonFunctionsSQL/src/commonfunctionssql/CommonFunctionsSQL.java
+++ CommonFunctionsSQL/src/commonfunctionssql/CommonFunctionsSQL.java
@@ -21,7 +21,38 @@ public class CommonFunctionsSQL {
     /**
      * @param args the command line arguments
      */
-    public static Connection makeConnect() throws SQLException {
+    public static Connection makeConnectMySQL() throws SQLException {
+        String connector = "jdbc:mysql";
+        String pc = "mysql.hostinger.es";
+        String port = "3306";
+        String bdName = "u571250913_cicl";
+        String user = "u571250913_ciclu";
+        String pass = "alumne";
+        String connection = connector + "://" + pc + ":" + port + "/" + bdName;
+        Connection c = DriverManager.getConnection(connection, user, pass);
+        return c;
+    }
+
+    public static Connection makeConnectMySQL(String bdName) throws SQLException {
+        String connector = "jdbc:mysql";
+        String pc = "mysql.hostinger.es";
+        String port = "3306";
+        String user = "u571250913_ciclu";
+        String pass = "alumne";
+        String connection = connector + "://" + pc + ":" + port + "/" + bdName;
+        Connection c = DriverManager.getConnection(connection, user, pass);
+        return c;
+    }
+
+    public static Connection makeConnectMySQL(String pc, String bdName, String user, String pass) throws SQLException {
+        String connector = "jdbc:mysql";
+        String port = "3306";
+        String connection = connector + "://" + pc + ":" + port + "/" + bdName;
+        Connection c = DriverManager.getConnection(connection, user, pass);
+        return c;
+    }
+
+    public static Connection makeConnectMariaDB() throws SQLException {
         String connector = "jdbc:mariadb";
         String pc = "10.0.211.181";
         String port = "3306";
@@ -33,7 +64,15 @@ public class CommonFunctionsSQL {
         return c;
     }
 
-    public static Connection makeConnect(String bdname) throws SQLException {
+    public static Connection makeConnectMariaDB(String pc, String bdName, String user, String pass) throws SQLException {
+        String connector = "jdbc:mariadb";
+        String port = "3306";
+        String connection = connector + "://" + pc + ":" + port + "/" + bdName + "?user=" + user + "&password=" + pass;
+        Connection c = DriverManager.getConnection(connection);
+        return c;
+    }
+
+    public static Connection makeConnectMariaDB(String bdname) throws SQLException {
         String connector = "jdbc:mariadb";
         String pc = "10.0.211.181";
         String port = "3306";
